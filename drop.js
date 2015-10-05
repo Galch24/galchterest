e02c0a9b5fc2e0b05195c592f26797c3ffaa64fc
Galina Golanova <g.golanova@leadsor.com>
2015-10-05 11:14:15 +0800

    Initialize commit.

---
+++ drop.js
@@ -0,0 +1,72 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var ReactClass = React.createClass({
+  
+  getInitialState: function () {
+        return {
+          isHeaderHidden: false
+        };
+      },
+  handleClick: function () {
+    this.setState({
+      isHeaderHidden: !this.state.isHeaderHidden
+    });
+  },
+  render: function () {
+    var title = 'Stateful React Component';
+    
+    var headerElement = React.createElement('h1', { className: 'header', key: 'header' }, title);
+    var buttonElement = React.createElement('button', { className: 'btn btn-default', onClick: this.handleClick, key: 'button' }, 'Toggle header');
+    if (this.state.isHeaderHidden) {
+      return React.createElement('div', null, [ buttonElement ]);
+    }
+    return React.createElement('div', null, [ buttonElement, headerElement ]);
+  }
+});
+var reactComponentElement = React.createElement(ReactClass);
+var reactComponent = ReactDOM.render(reactComponentElement, document.getElementById('react-application'));
+
+
+
+
+
+
+
+
+
+
+
+/*
+var listOfItems = <ul className="list-of-items">
+                    <li className="item-1">Item 1</li>
+                    <li className="item-2">Item 2</li>
+                    <li className="item-3">Item 3</li>
+                  </ul>;
+          
+ReactDOM.render(listOfItems, document.getElementById('react-application'));
+*/
+
+
+/*
+var listItemElement1 = React.DOM.li({ className: 'item-1', key: 'item-1' }, 'Item 1');
+var listItemElement2 = React.DOM.li({ className: 'item-2', key: 'item-2' }, 'Item 2');
+var listItemElement3 = React.DOM.li({ className: 'item-3', key: 'item-3' }, 'Item 3');
+var reactFragment = [ listItemElement1, listItemElement2, listItemElement3 ];
+var listOfItems = React.DOM.ul({ className: 'list-of-items' }, reactFragment);
+
+ReactDOM.render(listOfItems, document.getElementById('react-application'));*/
+
+
+/*
+
+var createListItemElement = React.createFactory('li');
+
+var listItemElement1 = createListItemElement({ className: 'item-1', key: 'item-1' }, 'Item 1');
+var listItemElement2 = createListItemElement({ className: 'item-2', key: 'item-2' }, 'Item 2');
+var listItemElement3 = createListItemElement({ className: 'item-3', key: 'item-3' }, 'Item 3');
+
+var reactFragment = [ listItemElement1, listItemElement2, listItemElement3 ];
+var listOfItems = React.createElement('ul', { className: 'list-ofitems' }, reactFragment);
+
+ReactDOM.render(listOfItems, document.getElementById('react-application'));*/
